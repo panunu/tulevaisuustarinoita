943de7318654afc0d16bacb74ecaa27a4ef9e7a7
Panu Leppäniemi <me@panuleppaniemi.com>
2014-12-13 20:59:47 +0200

    Refactorations

--- app/com/panuleppaniemi/services/DocumentService.scala
+++ app/com/panuleppaniemi/services/DocumentService.scala
@@ -1,28 +1,28 @@
 package com.panuleppaniemi.services
 
 import com.google.inject.Singleton
-import com.panuleppaniemi.models.{DocumentWithContent, Document}
-import play.api.libs.json.Json
+import com.panuleppaniemi.models.{DocumentWithContent, Document, Author}
+import play.api.libs.json.{JsValue, Json}
 import org.pegdown.PegDownProcessor
 
 @Singleton
 class DocumentService {
-  def all: List[DocumentWithContent] = {
-    val json = Json.parse(
-      scala.io.Source.fromFile("data/documents.json").mkString
-    )
+  def all: List[DocumentWithContent] = withParsedContent(toDocuments(parseJson))
 
-    val documents = json.validate[List[Document]].getOrElse(List())
+  private def parseJson = Json.parse(scala.io.Source.fromFile("data/documents.json").mkString)
 
-    documents.map(document => {
-      DocumentWithContent(
-        document,
-        parseMarkdown(document.filename)
-      )
-    })
+  private def toDocuments(json: JsValue) = {
+    implicit val authorFormat = Json.reads[Author]
+    implicit val documentFormat = Json.reads[Document]
+
+    json.validate[List[Document]].getOrElse(List())
   }
 
-  def parseMarkdown(filename: String): String = {
+  private def withParsedContent(documents: List[Document]) = documents.map(
+    document => DocumentWithContent(document, parseMarkdown(document.filename))
+  )
+
+  private def parseMarkdown(filename: String): String = {
     (new PegDownProcessor).markdownToHtml(
       scala.io.Source.fromFile("data/" + filename).mkString
     )
